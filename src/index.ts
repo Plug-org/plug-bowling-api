25ae457806aed985abfece6874e57c969adfa61f
Matt <84277551+MattRueter@users.noreply.github.com>
2025-05-22 23:58:51 -0500

    Create Database: - initialize database (at entry file for now) - create seed.js and cli script to seed DB - install cors and morgan - set up server and root GET route.

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,53 @@
-import GameEngine from "./gameEngine";
-
-const myGame = new GameEngine("_id1");
-
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-
-myGame.roll(10);
-myGame.roll(10);
-myGame.roll(10)
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const Database = require('better-sqlite3');
+const path = require('path');
+import type { Request, Response } from 'express';
 
 
+// Initialize DB
+const DB_FILE = './database.db'
+const db = new Database(DB_FILE);
+db.pragma('journal_mode = WAL');
 
-//console.log(myGame.getState())
-console.log(myGame.getState().frames)
-console.log("game finished ?: ", myGame.getState().isComplete)
-console.log("TOTAL: ",myGame.getTotal())
+//create users table
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+  );
+`).run();
+
+//create games table
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL,
+    frames TEXT NOT NULL,
+    current_frame_index INTEGER NOT NULL,
+    is_complete INTEGER NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  );
+`).run();
+
+// Start Express server
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+// Routes
+app.get('/users', (req: Request, res:Response) => {
+  const users = db.prepare('SELECT * FROM users ORDER BY created_at DESC').all();
+  res.json(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
