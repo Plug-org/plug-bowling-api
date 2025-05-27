81fbea2ae971a3037d7ad19828bd267c824f7e0e
Matt <84277551+MattRueter@users.noreply.github.com>
2025-05-27 15:43:53 -0500

    add create new game route.

--- src/routes/games.route.ts
+++ src/routes/games.route.ts
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require('../db/init');
 import type { Request, Response } from 'express';
+const GameEngine =require('../gameEngine');
 
 
 const router = express.Router();
@@ -19,6 +20,33 @@ router.get("/:id", (req:Request, res:Response) => {
   }else{
     res.status(404).json({ error: "Game not found." })
   }
+});
+
+router.post("/", (req:Request, res:Response) => {
+  const { userId } = req.body;
+  
+  // create game instance
+  const game = new GameEngine(userId);
+  const { id, frames, currentFrameIndex, isComplete } = game.state;
+
+  //insert game into db
+  const stmt = db.prepare((`
+    INSERT INTO games (
+    user_id, frames, current_frame_index, is_complete
+    ) VALUES (?,?,?,?)
+  `));
+  const result = stmt.run(
+    //id,
+    userId,
+    JSON.stringify(frames),
+    currentFrameIndex,
+    isComplete ? 1 : 0
+   );
+
+   //respond with new game blank state 
+   const newGame = db.prepare('SELECT * FROM games WHERE id = ?').get(result.lastInsertRowid);
+   res.json(newGame);
+
 });
 
 module.exports = router;
