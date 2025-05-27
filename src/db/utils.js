c4088c0bfdb7efc88e421099b43e46a9a1730c1e
Matt <84277551+MattRueter@users.noreply.github.com>
2025-05-27 14:49:06 -0500

    update seeding games script to preserve user id when auto increments happen.

--- src/db/utils.js
+++ src/db/utils.js
@@ -6,10 +6,11 @@ const DB_FILE = path.resolve(__dirname, '../../database.db');
 const db = new Database(DB_FILE);
 
 
-//read initial data from local users JSON file and seed DB
 function seedUsers() {
+    //read file
     const filePath = path.join(__dirname, "/seed_users.json")
     const seedData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    //populate user table
     const insert = db.prepare('INSERT INTO users (name ) VALUES (?)');
     const insertMany = db.transaction((users) => {
       for (const user of users) insert.run(user.name);
@@ -19,19 +20,28 @@ function seedUsers() {
 };
 
 
-//read initial data from local games JSON file and seed DB
 function seedGames() {
+    //read file
     const seedGamesFile = path.join(__dirname, 'seed_games.json');
     const gamesData = JSON.parse(fs.readFileSync(seedGamesFile, 'utf-8'));
+
+    // get seeded user ids in order to preserve "ownership" 
+    // of seeded games by users when id auto-increments on 'npm run reset'.
+    // This way they stay in sync over time.
+    const getUserIds = db.prepare('SELECT id FROM users ORDER BY id ASC').all();
+    const userIds = getUserIds.map(user => user.id);
+
+    //populate games table
     const insertGame = db.prepare(`
       INSERT INTO games (
         user_id, frames, current_frame_index, is_complete 
         ) VALUES (?, ?, ?, ?)
       `);
-
-    gamesData.forEach((game) => {
+          
+    gamesData.forEach((game, index) => {
+      const userId = userIds[index];
       insertGame.run(
-        game.userId,
+        userId,
         JSON.stringify(game.frames),
         game.currentFrameIndex,
         game.isComplete ? 1 : 0
